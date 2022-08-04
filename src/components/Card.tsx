005ad41c7cef69bef3c0110ccab6474d4c317090
InigoGastesi <inigogastesi@gmail.com>
2022-08-04 13:10:03 +0200

    algunos ajustes a cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,18 @@
 import { ICard } from "../interface/jsondata";
-import { Element } from "react-scroll"
 
 export function Card({ titulo, info, images }: ICard) {
   return (
     
     <div
-      className={`rounded-lg drop-shadow-md py-20 min-h-80 w-full`}
+      className={`rounded-lg drop-shadow-md min-h-80 py-10 w-full`}
       style={{ backgroundImage: `url(${images[0]})`, backgroundSize: "cover" }}
     >
       
-      <div className="flex flex-col font-poppins w-1/2 p-8">
-        <div>{titulo}</div>
-        <div>
+      <div className="md:flex flex-col font-poppins w-1/2 p-8">
+        <h1 className="font-poppins font-bold uppercase">{titulo}</h1>
+        <p>
           {info}
-        </div>
+        </p>
       </div>
     </div>
   );
