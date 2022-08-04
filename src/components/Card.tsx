dd1efa0079768c6daa663d7d7c443157fc3a23e4
InigoGastesi <inigogastesi@gmail.com>
2022-08-04 09:16:03 +0200

    scroll done

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,14 @@
 import { ICard } from "../interface/jsondata";
-
+import { Element } from "react-scroll"
 
 export function Card({ titulo, info, images }: ICard) {
   return (
+    
     <div
       className={`rounded-lg drop-shadow-md py-20 min-h-80 w-full`}
       style={{ backgroundImage: `url(${images[0]})`, backgroundSize: "cover" }}
     >
+      
       <div className="flex flex-col font-poppins w-1/2 p-8">
         <div>{titulo}</div>
         <div>
