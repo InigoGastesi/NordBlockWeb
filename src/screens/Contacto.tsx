65df0209fbef7f589f91e77199ae8b0d7137aef8
InigoGastesi <inigogastesi@gmail.com>
2022-08-04 07:22:28 +0200

    add forms

---
+++ src/screens/Contacto.tsx
@@ -0,0 +1,19 @@
+import { Formik } from "formik";
+
+export function Contacto() {
+  return (
+    <div className="w-screen h-screen grid grid-flow-col justify-center">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLScsjJScEOfD2p36pAoFn2ZA1RZUS0RmGpYQd8JzpIXcXFQ7iw/viewform?embedded=true"
+        width="640"
+        height="542"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+        className="h-screen"
+      >
+        Cargando…
+      </iframe>
+    </div>
+  );
+}
---
+++ src/screens/ProductInfo.tsx
@@ -0,0 +1,32 @@
+import { Navbar, Card, Minicard, Productdescriptor } from "../components";
+import Context from "../context/Context";
+import { useContext } from "react";
+import { IScreen } from "../interface/jsondata";
+
+
+export function ProductInfo({screen, titulo}:IScreen) {
+  const screenInfo = useContext(Context);
+  let data = screenInfo?.find((e) => e.screen == screen);
+
+  let cards = data?.cards;
+  return (
+    <div>
+      <h1 className="font-poppins py-8 px-32 text-blue font-bold text-3xl">{titulo}</h1>
+      <div className="grid grid-cols-3 w-screen gap-20 p-8 px-80">
+        {cards?.map(({ titulo, info, images }) => {
+          return (
+            <Minicard titulo={titulo} info={info} images={images}></Minicard>
+          );
+        })}
+      </div>
+      <div className="grid grid-cols-1 w-screen gap-8 p-8 px-80 h-screen">
+        {cards?.map(({ titulo, info, images }) => {
+          return (
+            <Productdescriptor titulo={titulo} info={info} images={images}></Productdescriptor>
+          );
+        })}
+      </div>
+      
+    </div>
+  );
+}
