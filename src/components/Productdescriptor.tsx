8a834c64128c51475ab0bce5e961c04e6a198062
InigoGastesi <inigogastesi@gmail.com>
2022-08-06 10:45:24 +0200

    imagenes version 2

--- src/components/Productdescriptor.tsx
+++ src/components/Productdescriptor.tsx
@@ -3,11 +3,14 @@ import { ICard } from "../interface/jsondata";
 export function Productdescriptor({ titulo, info, images }: ICard) {
   return (
     <div className="xl:flex xl:flex-row xl:gap-x-8 w-full drop-shadow-md rounded-md bg-gray-100 justify-between">
-      <img
-        src={images![0]}
-        className="hidden xl:block object-fill w-0 xl:w-1/2 aspect-square rounded-l-md drop-shadow-md"
-      ></img>
-
+      <div
+        className="rounded-lg drop-shadow-md px-72 justify-between"
+        style={{
+          backgroundImage: `url(${images![0]})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center"
+        }}
+        ></div>
       <div className="font-poppins py-4 xl:w-1/2 w-full p-4 mr-4">
         <h1 className="font-bold text-xl text-blue">{titulo}</h1>
         <p className="pt-4">{info}</p>
