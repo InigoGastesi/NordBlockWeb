3b19bd4f478c972373fb0936d760e42120f368fd
Iñigo Gastesi <inigogastesi@gmail.com>
2025-02-11 20:06:53 +0100

    add junta logo and document to footer; update package.json with react-scripts

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,6 @@
 import logolarge from "../assets/NordBlock_-_Horizontal_-_Full_color_-_Digital_-_Small_-_Alpha.svg";
+import juntaLog from "../assets/junta_sub.jpg";
+import juntaDoc from '../docs/CARTEL FEDER 21-27-CREACIÓN EMPRESAS- A3ICE.pdf';
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import Context from "../context/Context";
@@ -9,12 +11,19 @@ export function Footer() {
   const navigate = useNavigate();
   return (
     <div className="font-poppins w-full text-pink py-8 px-2 bg-blue mt-auto">
-      <div className="grid grid-cols-1 lg:grid-cols-3 pt-4">
+      <div className="grid grid-cols-1 lg:grid-cols-4 pt-4">
         <div>
           <img
             src={logolarge}
             onClick={() => navigate("/")}
             className="hidden lg:block"
+          />
+        </div>
+        <div className="pr-14">
+          <img
+            src={juntaLog}
+            onClick={() => window.open(juntaDoc, '_blank')}
+            className="hidden lg:block cursor-pointer"
           />
         </div>
         <div>
@@ -50,21 +59,21 @@ export function Footer() {
                 Burgos
               </a>
             </li>
-            <li className="text-white flex flex-cols py-1">
+            {/* <li className="text-white flex flex-cols py-1">
               <div>
                 <PhoneIcon className="w-6"></PhoneIcon>
               </div>
-              <p className="pl-3">+34 622 06 87 73</p>
-            </li>
+              <p className="pl-3">+34 655 70 01 21</p>
+            </li> */}
             <li className="text-white flex flex-cols py-1">
               <div>
                 <MailIcon className="w-6"></MailIcon>
               </div>
               <a
                 className="pl-3 cursor-pointer"
-                href="mailto:jccardenas@nord-block.com"
+                href="mailto:igastesi@nord-block.com"
               >
-                jccardenas@nord-block.com
+                igastesi@nord-block.com
               </a>
             </li>
           </ul>
